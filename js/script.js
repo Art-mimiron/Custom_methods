e4f59a8217dd67809fe5ee27a1a4139efc889bb1
Artem <artmimiron@gmail.com>
2020-10-20 19:53:38 +0300

    +customFilter()

--- js/script.js
+++ js/script.js
@@ -59,6 +59,29 @@ document.addEventListener('DOMContentLoaded', function() {
       ];
       
       let user = users.customFindIndex(users => users.name === 'Петя');
-      
       console.log(user);
 });
+
+//filter()
+    Object.prototype.customFilter = function(callback, thisContext) {
+        let array = Object(this),
+            value,
+            result = [];
+            thisContext = arguments[1];
+
+            for (let i = 0; i < array.length; i++) {
+                value = array[i];
+                if (callback(value, i, array, thisContext)) {
+                    result.push(value);
+                }
+            }
+        return result;
+    };
+
+//test 1 
+const words = ['spray', 'limit', 'elite', 'exuberant', 'destruction', 'present'];
+console.log(words.customFilter(word => word.length > 6));
+
+//test 2
+const nums = [4, 2, 13, 7, 9, 993, 16754, 123, 44];
+console.log(nums.customFilter(number => number % 2 === 0));
